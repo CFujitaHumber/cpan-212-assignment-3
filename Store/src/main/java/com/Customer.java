75729c5dac47cd3477611a943ea47bfd0e9bd2d2
TheFujirose <TheFujirose@users.noreply.github.com>
2025-11-11 16:42:26 -0500

    inital classes

---
+++ Store/src/main/java/com/Customer.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package main.java.com;
+
+/**
+ * 
+ */
+public class Customer implements Comparable<Customer> {
+
+	/**
+	 * 
+	 */
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Customer o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Store/src/main/java/com/Product.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package main.java.com;
+
+/**
+ * 
+ */
+public class Product implements Comparable<Product>{
+	
+	/**
+	 * 
+	 */
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Product o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}	
+
+}
